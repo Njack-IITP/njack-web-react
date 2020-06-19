8885c63165a56702c1e65f5b9d2eb2e137d650fe
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-06-19 17:54:56 +0530

    added footer component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react'
 import About from './About'
 import Events from './Events'
 import Collab from './Collab'
-
+import Footer from './Footer'
 export default class Home extends Component {
     render() {
         return (
             <div className="container-fluid main-container">
+                <div className="sidebaricon d-none d-lg-block">
+                    <img src={require('../assets/Instagram – 1.png')} /><br /><br />
+                    <img src={require('../assets/Facebook – 1.png')} /><br /><br />
+                    <img src={require('../assets/Twitter – 1.png')} /><br /><br />
+                    <img src={require('../assets/Github – 1.png')} />
+                </div>
                 <About />
                 <Events />
                 <Collab />
+                <Footer />
             </div>
         )
     }
