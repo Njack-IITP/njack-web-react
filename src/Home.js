249c8e8b2d0614527078f78fe3a28298165a00a9
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-06-19 11:46:16 +0530

    removed old images folder

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,4 @@
 import React, { Component } from 'react'
-import TopNav from './TopNav'
-import { SocialMediaIconsReact } from 'social-media-icons-react';
-import recImage from './images/recImage.png'
 
 export default class Home extends Component {
     render() {
