7830a5a37eca0fbf48ef56992f733c2eacc2a60a
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-06-20 00:41:08 +0530

    added gsocers thumbnails

--- src/components/Gsoc.js
+++ src/components/Gsoc.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 
 export default class Gsoc extends Component {
-    
+
     render() {
         return (
             <div>
@@ -28,6 +28,31 @@ export default class Gsoc extends Component {
                         <button className="btn btn-danger gsoc-results">View Results</button>
                     </div>
                 </div>
+
+                {/* <!-- visible on md and above --> */}
+                <div class="container-fluid d-none d-md-block">
+                    <div class="row gsoc justify-content-center">
+                        <div class="col-md-3 magic">
+                            <img class="img-fluid magic-img" src={require('../assets/Ellipse_49_d.png')} />
+                            <p class="gsoc-para">
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    </p>
+                        </div>
+                        <div class="col-md-3 magic">
+                            <img class="img-fluid magic-img" src={require('../assets/Ellipse_49_ea.png')} />
+                            <p class="gsoc-para">
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    </p>
+                        </div>
+                        <div class="col-md-3 magic">
+                            <img class="img-fluid magic-img" src={require('../assets/Ellipse_49.png')} />
+                            <p class="gsoc-para">
+                                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                    </p>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         )
     }
