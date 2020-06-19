8ec10685424aa8b4709d776ba630606429f0a984
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-06-19 17:00:16 +0530

    added events component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
 import About from './About'
+import Events from './Events'
 export default class Home extends Component {
     render() {
         return (
-            <About />
+            <div className="container-fluid main-container">
+                <About />
+                <Events />
+
+            </div>
         )
     }
 }
