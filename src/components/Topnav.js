1364c3cbdac6d8ffcad98189144baa64a9387de4
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-06-24 13:15:46 +0530

    Add functional  navbar with animation

--- src/components/Topnav.js
+++ src/components/Topnav.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react'
 
 export default class Topnav extends Component {
+
+    state={
+        isVisible: false
+    };
+    
+    handleClick = ()=>{
+        this.setState( prevState => ({
+               isVisible: !prevState.isVisible
+        }));
+        console.log(this.state);
+    };
+
     render() {
+        const isVisible = this.state.isVisible;
         return (
             // <!-- navbar -->
             <nav>
-                <div class="logo">
+                <div className="logo">
                     <h4>NJACK</h4>
                 </div>
-                <ul class="nav-links">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Events</a></li>
-                    <li><a href="#">Projects</a></li>
+                <ul className={ `nav-links ${isVisible ? "nav-active" : "" }` }>
+                    <li className={isVisible ? "nav-links-animation" : ""}><a href="#">Home</a></li>
+                    <li className={isVisible ? "nav-links-animation" : ""}><a href="#">About</a></li>
+                    <li className={isVisible ? "nav-links-animation" : ""}><a href="#">Events</a></li>
+                    <li className={isVisible ? "nav-links-animation" : ""}><a href="#">Projects</a></li>
                 </ul>
 
-                <div class="burger">
-                    <div class="line1"></div>
-                    <div class="line2"></div>
-                    <div class="line3"></div>
+                <div className={ `burger ${isVisible ? "toggle" : "" }` } onClick={this.handleClick}>    
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
                 </div>
             </nav>
         )
