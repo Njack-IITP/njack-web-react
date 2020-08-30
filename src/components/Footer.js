507fc90546206d01613976280a481240b3bfd7b6
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-08-30 19:41:20 +0530

    Add text and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react'
 
 export default class Footer extends Component {
-    render() {
-        return (
-            <div class="container-fluid">
-                <div class="row foot-row">
+  render() {
+    return (
+      <div class="container-fluid">
+        <div class="row foot-row">
 
-                    <div class="col-md-4">
-                        <h4>NJACK</h4>
-                        <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+          <div class="col-md-4">
+            <h4>NJACK</h4>
+            <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                                 tempor incididunt ut labore et dolore magna aliqua.</b>
-                        <br />
-                        <br />
-                        <img height="36px" width="36px" src={require('../assets/git.png')} />
-                        <img height="36px" width="36px" src={require('../assets/IG.png')} />
-                        <img height="36px" width="36px" src={require('../assets/TWITTER.png')} />
+            <br />
+            <br />
+            <a href="https://github.com/Njack-IITP"><img height="36px" width="36px" src={require('../assets/git.png')} /></a>
+            <a href="https://www.instagram.com/iit_patna/"><img height="36px" width="36px" src={require('../assets/IG.png')} /></a>
+            <a href="https://twitter.com/njackiitp"><img height="36px" width="36px" src={require('../assets/TWITTER.png')} /></a>
+          </div>
+        </div>
+      </div>
 
-                    </div>
-                </div>
-            </div>
-
-        )
-    }
+    )
+  }
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,29 @@
 import React, { Component } from 'react'
 import Homepage from './Homepage'
-import Topnav from './Topnav'
 import About from './About'
 import Events from './Events'
 import Collab from './Collab'
 import Footer from './Footer'
 import Gsoc from './Gsoc'
-export default class Home extends Component {
-    render() {
-        return (
-            <div>
-                <Topnav />
-                <div className="container-fluid main-container">
-                    <div className="sidebaricon d-none d-lg-block">
-                        <img src={require('../assets/Instagram – 1.png')} /><br /><br />
-                        <img src={require('../assets/Facebook – 1.png')} /><br /><br />
-                        <img src={require('../assets/Twitter – 1.png')} /><br /><br />
-                        <img src={require('../assets/Github – 1.png')} />
-                    </div>
-                    <Homepage />
-                    <About />
-                    <Events />
-                    <Gsoc />
-                    <Collab />
-                    <Footer />
-                </div>
-            </div>
 
-        )
-    }
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="container-fluid main-container">
+        <div className="sidebaricon d-none d-lg-block">
+          <a href="https://www.instagram.com/iit_patna/"><img src={require('../assets/Instagram – 1.png')}/></a><br /><br />
+          <a href="https://www.facebook.com/njack.iitp/"><img src={require('../assets/Facebook – 1.png')} /></a><br /><br />
+          <a href="https://twitter.com/njackiitp"><img src={require('../assets/Twitter – 1.png')} /></a><br /><br />
+          <a href="https://github.com/Njack-IITP"><img src={require('../assets/Github – 1.png')} /></a>
+        </div>
+        <Homepage />
+        <About />
+        <Events />
+        <Gsoc />
+        <Collab />
+        <Footer />
+      </div>
+
+    )
+  }
 }
