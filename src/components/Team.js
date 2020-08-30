822b96907c659382275171adce699ac312d78c0b
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-08-31 00:29:41 +0530

    Add team page

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
+import { team, fields } from './../data/teamData';
+import Profile from './common/profile';
 
 export default class Team extends Component {
-    render() {
-        return (
-            <div id="team=page">
-                <h1>Teams Page</h1> 
-            </div>
-        )
-    }
+  state = {
+    team,
+    fields
+  }
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col"></div>
+          <div className="col-7 col-sm-5 col-md-3 about">
+            <img
+              src={require('../assets/team.svg')}
+              alt="team heading"
+              className="aboutus img-fluid"
+              style={{ marginTop: "57px" }}
+            />
+          </div>
+          <div className="col"></div>
+        </div>
+        {this.state.fields.map(f => {
+          let t = this.state.team.filter(m => m.field === f);
+          return(<div className="row justify-content-center team-row">
+            {t.map(m =>
+              (<Profile
+                name={m.name}
+                position={m.position}
+                field={m.field}
+                git={m.git}
+                fb={m.fb}
+                lin={m.lin}
+                imgLink={'./profileImages/Ellipse_49_d.png'}
+              />))}
+          </div>)
+        })}
+
+      </div>
+    )
+  }
 }
--- src/components/common/profile.js
+++ src/components/common/profile.js
@@ -4,14 +4,14 @@ export default function Profile(props) {
     const {name, position, field, git, fb, lin, imgLink} = props;
     return (
         <div className="col-md-3 profile">
-            <img className="img-fluid profile-img" src={require(''+imgLink)} />
+            <img className="img-fluid profile-img" src={require(''+imgLink)} alt="profile photo"/>
             <p className="profile-details">
                 {name}<br/>
                 {position}<br/>
                 {position!=="Overall Co-ordinator" && field}<br/>
-                <a href={lin}><i className="fa fa-linkedin-square fa-lg" style={{color:"white"}}/></a>
-                <a href={git}><i className="fa fa-github-square fa-lg m-2" style={{color:"white"}}/></a>
-                <a href={fb}><i className="fa fa-facebook-square fa-lg" style={{color:"white"}}/></a>
+                {lin && <a href={lin}><i className="fa fa-linkedin-square fa-lg m-2" style={{color:"white"}}/></a>}
+                {git && <a href={git}><i className="fa fa-github-square fa-lg m-2" style={{color:"white"}}/></a>}
+                {fb && <a href={fb}><i className="fa fa-facebook-square fa-lg m-2" style={{color:"white"}}/></a>}
             </p>
         </div>
     )
