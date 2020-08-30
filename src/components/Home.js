d5d06d3e8a4f725c4781234f8dcb9a613e8f08d8
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-08-30 21:15:51 +0530

    Refactor Code

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,17 +5,13 @@ import Events from './Events'
 import Collab from './Collab'
 import Footer from './Footer'
 import Gsoc from './Gsoc'
+import Sidebar from './Sidebar'
 
 export default class Home extends Component {
   render() {
     return (
       <div className="container-fluid main-container">
-        <div className="sidebaricon d-none d-lg-block">
-          <a href="https://www.instagram.com/iit_patna/"><img src={require('../assets/Instagram – 1.png')}/></a><br /><br />
-          <a href="https://www.facebook.com/njack.iitp/"><img src={require('../assets/Facebook – 1.png')} /></a><br /><br />
-          <a href="https://twitter.com/njackiitp"><img src={require('../assets/Twitter – 1.png')} /></a><br /><br />
-          <a href="https://github.com/Njack-IITP"><img src={require('../assets/Github – 1.png')} /></a>
-        </div>
+        <Sidebar/>
         <Homepage />
         <About />
         <Events />
