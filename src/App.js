3be5ff2b3e261c8af6153ef0bf2c2e4bd890d0a4
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-09-02 02:23:22 +0530

    Add dark mode button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from './components/Home'
 import Topnav from './components/Topnav';
@@ -6,9 +6,11 @@ import Team from './components/Team';
 import './App.css';
 
 function App() {
+  const [darkMode, setDarkMode]  = useState(0); 
+  const handleDarkModeChange = ()=> setDarkMode(darkMode^1);
   return (
-    <div className="App">
-      <Topnav/>
+    <div className={darkMode?"App dark":"App"}>
+      <Topnav onDarkModeToggle={handleDarkModeChange}/>
       <Switch>
        <Route path="/team" component={Team} />
        <Route path="/" component={Home} />
