2d76f5978b9b72d6dd9e537f70a4a7601b8cda5c
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-09-06 02:03:15 +0530

    Add gsoc results

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Home from './components/Home'
+import GsocPage from './components/GsocPage'
 import Topnav from './components/Topnav';
 import Team from './components/Team';
 import './App.css';
@@ -13,6 +14,7 @@ function App() {
       <Topnav onDarkModeToggle={handleDarkModeChange}/>
       <Switch>
        <Route path="/team" render={()=><Team darkMode={darkMode}/>} />
+       <Route path="/gsoc" render={()=><GsocPage darkMode={darkMode}/>} />
        <Route path="/" render={()=><Home darkMode={darkMode}/>} />
       </Switch>
     </div>
--- src/components/common/profile.js
+++ src/components/common/profile.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 export default function Profile(props) {
-    const {name, position, field, git, fb, lin, imgLink} = props;
+    const {name, org, position, field, git, fb, lin, imgLink} = props;
     return (
-        <div className="col-md-3 profile">
+        <div className="col-md-3 profile" style={{margin:"30px"}}>
             <img className="img-fluid profile-img" src={require(''+imgLink)} alt="profile photo"/>
             <p className="profile-details">
                 {name}<br/>
-                {position}<br/>
-                {position!=="Overall Co-ordinator" && field}<br/>
+                {org}{org && <br/>}
+                {position}{position && <br/>}
+                {position!=="Overall Co-ordinator" && field}{field && <br/>}
                 {lin && <a href={lin} target="_blank"><i className="fa fa-linkedin-square fa-lg m-2" style={{color:"white"}}/></a>}
                 {git && <a href={git} target="_blank"><i className="fa fa-github-square fa-lg m-2" style={{color:"white"}}/></a>}
                 {fb && <a href={fb} target="_blank"><i className="fa fa-facebook-square fa-lg m-2" style={{color:"white"}}/></a>}
