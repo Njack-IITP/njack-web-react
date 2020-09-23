1d0a216605d3ef01a2838a17315a48b249233d62
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-09-23 20:23:11 +0530

    Embed facebook page

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class Homepage extends Component {
   render() {
-    const {darkMode} = this.props;
+    const { darkMode } = this.props;
     return (
       // <!-- Home -->
       <div className="container-fluid main-homepage-container" >
@@ -12,9 +12,9 @@ export default class Homepage extends Component {
           <div className="col-md-5 njack-block">
             <img className="njack-logo" src={require('../assets/njack-logo.svg')} alt="njack-logo" />
             <br /><br />
-            <h2 className={darkMode?"not-just-text dark-text-heading":"not-just-text"}>Not Just Another <br />ComputerScience Klub</h2>
+            <h2 className={darkMode ? "not-just-text dark-text-heading" : "not-just-text"}>Not Just Another <br />ComputerScience Klub</h2>
             <br />
-            <p className={darkMode?"home-para dark-text-para":"home-para"}>
+            <p className={darkMode ? "home-para dark-text-para" : "home-para"}>
               The Computer Science Club of IIT Patna!
             </p>
             <button className="btn home-but">
@@ -32,24 +32,38 @@ export default class Homepage extends Component {
         </div>
 
         <div className="row justify-content-center angbrack-row">
-          <div className="col-md-3 home-clip">
-            <img className="home-clip-img" src={require('../assets/angbrack.svg')} />
-            <p className="angbrack-para">We develop amazing web and mobile apps!</p>
+          <div className="col" style={{padding:"0"}}>
+          <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fnjack.iitp%2F&tabs=timeline&width=340&height=540&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="540" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
           </div>
-          <div className="col-md-3 home-clip">
-            <img src={require('../assets/angbrack.svg')} />
-            <p className="angbrack-para">We innovate in the fields of AI & Machine Learning!</p>
-          </div>
-          <div className="col-md-3 home-clip">
-            <img src={require('../assets/angbrack.svg')} />
-            <p className="angbrack-para">We compete zealously in Programming Contests and Hackathons!</p>
-          </div>
+          {/* <div className="col-md-5">
+            <div className="row">
+              <div className="col home-clip">
+                <img className="home-clip-img" src={require('../assets/angbrack.svg')} />
+                <p className="angbrack-para">We develop amazing web and mobile apps!</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col home-clip">
+                <img src={require('../assets/angbrack.svg')} />
+                <p className="angbrack-para">We innovate in the fields of AI & Machine Learning!</p>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col home-clip">
+                <img src={require('../assets/angbrack.svg')} />
+                <p className="angbrack-para">We compete zealously in Programming Contests and Hackathons!</p>
+              </div>
+            </div>
+          </div> */}
 
         </div>
+
+
+
         <br />
         <hr className="nwoc-aper-line" />
         <br /><br />
-      </div>
+      </div >
     )
   }
 }
