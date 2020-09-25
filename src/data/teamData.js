766aa798be28098f47449049bd04e143dbe0e8d6
aradhya-gupta <aradhya.gupta0308@gmail.com>
2020-09-25 11:32:03 +0530

    Add image links to data

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -5,7 +5,8 @@ export const team = [
         field:"Overall", 
         git:"https://github.com/abhishekchopra13/", 
         fb:"https://www.facebook.com/abhishekchopra13", 
-        lin:"https://www.linkedin.com/in/abhiii13/"
+        lin:"https://www.linkedin.com/in/abhiii13/",
+        imgLink:'./profileImages/abhishek.svg'
     },
     {
         name:"Anmol‌ Chaddha", 
@@ -13,7 +14,8 @@ export const team = [
         field:"Development &‌ Open Source", 
         git:"https://github.com/chanmol1999", 
         fb:"https://www.facebook.com/anmol.chaddha.125", 
-        lin:"https://www.linkedin.com/in/anmol-chaddha/"
+        lin:"https://www.linkedin.com/in/anmol-chaddha/",
+        imgLink:'./profileImages/anmol.svg'
     },
     {
         name:"Ashwani Yadav", 
@@ -21,7 +23,8 @@ export const team = [
         field:"Development &‌ Open Source", 
         git:"https://github.com/ashwaniYDV/", 
         fb:"https://www.facebook.com/ashwani.yadav9499", 
-        lin:"https://www.linkedin.com/in/ashwaniydv/"
+        lin:"https://www.linkedin.com/in/ashwaniydv/",
+        imgLink:'./profileImages/ashwani.svg'
     },
     {
         name:"Amish Mittal", 
@@ -29,7 +32,8 @@ export const team = [
         field:"Machine Learning", 
         git:"https://github.com/fliptrail", 
         fb:"https://www.facebook.com/profile.php?id=100019222320303", 
-        lin:"https://www.linkedin.com/in/amishmittal/"
+        lin:"https://www.linkedin.com/in/amishmittal/",
+        imgLink:'./profileImages/amish.svg'
     },
     {
         name:"Nischal Ashok", 
@@ -37,7 +41,8 @@ export const team = [
         field:"Machine Learning", 
         git:"https://github.com/Nish-19", 
         fb:"https://www.facebook.com/nischal.ashok.3", 
-        lin:"https://www.linkedin.com/in/nischal-a-67541616a/"
+        lin:"https://www.linkedin.com/in/nischal-a-67541616a/",
+        imgLink:'./profileImages/nischal.svg'
     },
     {
         name:"Kunj‌ Taneja", 
@@ -45,7 +50,8 @@ export const team = [
         field:"Competitive Programming", 
         git:"", 
         fb:"https://www.facebook.com/kunj.taneja.5", 
-        lin:"https://www.linkedin.com/in/kunjtaneja/"
+        lin:"https://www.linkedin.com/in/kunjtaneja/",
+        imgLink:'./profileImages/kunj.svg'
     },
     {
         name:"Shresth‌‌‌‌ Walia", 
@@ -53,7 +59,8 @@ export const team = [
         field:"Competitive Programming", 
         git:"", 
         fb:"https://www.facebook.com/shresthawalia", 
-        lin:"https://www.linkedin.com/in/shresth-walia-09a34b170"
+        lin:"https://www.linkedin.com/in/shresth-walia-09a34b170",
+        imgLink:'./profileImages/shresth.svg'
     },
     {
         name:"G Maheswar‌‌‌‌‌ Reddy‌‌‌‌‌‌", 
@@ -61,13 +68,15 @@ export const team = [
         field:"Cyber Security", 
         git:"", 
         fb:"https://www.facebook.com/g.maheswarreddy.3", 
-        lin:""
+        lin:"",
+        imgLink:'./profileImages/placeholder.svg'
     }
 ]
 
 export const fields = [
     "Overall",
     "Development &‌ Open Source",
+    "Machine Learning",
     "Cyber Security",
     "Competitive Programming"
 ]
